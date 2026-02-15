9483ddf2eb166b5b76856f356a823383ecc5c80b
Nikos Koutsolelos <70406237+5okin@users.noreply.github.com>
2026-02-15 21:40:44 +0000

    feat: add x logo instead of twitter

--- animations/typingEffect.js
+++ animations/typingEffect.js
@@ -2,23 +2,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const textElement = document.getElementById("bot-type");
 
     const botTypes = [
-        { name: "Twitter", color: "#1DA1F2" },
+        { name: "Twitte", color: "#1DA1F2" },
+        { name: "𝕏"     , color: "#1DA1F2" },
         { name: "Discord", color: "#5865F2" },
         { name: "Bluesky", color: "#0095FF" }
     ];
     
-    let index = 1;
+    let index = 0;
     let charIndex = 0;
-    let isDeleting = false;
-    let typingSpeed = 100;
-    let pauseTime = 1000;
     let cursorVisible = true;
+    let isDeleting = false;
+    const typingSpeed = 100;
+    const pauseTime = 1000;
+    const shortPauseTime = 100
+    
 
     function typeEffect() {
-        let currentText = botTypes[index].name;
-        let cursor = `<span class="cursor" style="opacity: ${cursorVisible ? "1" : "0"}">_</span>`;
+        const chars = Array.from(botTypes[index].name);
+        const cursor = `<span class="cursor" style="opacity: ${cursorVisible ? "1" : "0"}">_</span>`;
 
-        textElement.innerHTML = `I'm a <span style="color: ${botTypes[index].color}">${currentText.substring(0, charIndex)}</span>${cursor}<span class="text-white">bot</span>`;
+        textElement.innerHTML = `I'm a <span style="color: ${botTypes[index].color}">${chars.slice(0, charIndex).join("")}</span>${cursor}<span class="text-white">bot</span>`;
 
         if (isDeleting) {
             charIndex--;
@@ -26,8 +29,9 @@ document.addEventListener("DOMContentLoaded", function () {
             charIndex++;
         }
 
-        if (!isDeleting && charIndex === currentText.length) {
-            setTimeout(() => isDeleting = true, pauseTime);
+        if (!isDeleting && charIndex === chars.length) {
+            const pause = index === 0 ? shortPauseTime : pauseTime;
+            setTimeout(() => isDeleting = true, pause);
         } else if (isDeleting && charIndex === 0) {
             isDeleting = false;
             index = (index + 1) % botTypes.length;
